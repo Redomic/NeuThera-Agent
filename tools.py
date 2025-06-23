5bd7a7ec974b0d610e60a70fb1fd8a9e01a6cdb0
Redomic <redomicmc@gmail.com>
2025-06-23 21:07:43 +0530

    added license

--- tools.py
+++ tools.py
@@ -40,17 +40,6 @@ from pyvis.network import Network
 
 import boto3
 
-# load_dotenv()
-# arango_graph = ArangoGraph(db)
-# bedrock_client = boto3.client(
-#     "bedrock-runtime",
-#     region_name=os.getenv("AWS_REGION"),
-#     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-#     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-# )
-
-# llm = Bedrock(client=bedrock_client, model_id="mistral.mistral-large-2402-v1:0")
-
 # ================= Functions =================
 
 def FindDrug(drug_name: str):
